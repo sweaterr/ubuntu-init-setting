fb630f217e1952121b2826d7afebd548a68324bb
will.link <will.link@daumkakao.com>
2016-05-21 23:41:39 +0900

    first

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+test_nvidia()
+{
+    nvcc --version
+    nvidia-smi
+
+    python -c "import theano"
+    python -c "import theano; print(theano.__file__)"
+    python -c "import cv2; print(cv2.__file__)"
+    python -m ibeis_cnn
+    python -m ibeis_cnn._plugin --test-generate-species-background:0 --show
+
+    # May need to recompile theano and pylearn2
+    # git rm -rf * in the code dir
+    # then sudo python setup.py develop
+}
+
+test_nvidia()
